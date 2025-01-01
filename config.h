7e6c13f4760a586a82de88a52c12777dee64c2ae
Rumen <rumenmitov@protonmail.com>
2025-01-02 00:59:04 +0100

    ligature font + rm junk

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=24:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=24:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"Hack Nerd Font Mono:pixelsize=24:antialias=true:autohint=true", 
