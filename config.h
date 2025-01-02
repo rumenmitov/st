129e1350ad507c69188285cabcdc271435460bc3
Rumen <rumenmitov@protonmail.com>
2025-01-02 23:58:04 +0100

    config.h: switching to fira code

--- config.h
+++ config.h
@@ -5,12 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font Mono:pixelsize=24:antialias=true:autohint=true";
+static char *font = "Fira Code:pixelsize=24:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"CaskaydiaCove Nerd Font:pixelsize=24:antialias=true:autohint=true", 
     "Noto Color Emoji:pixelsize=24:antialias=true:autohint=true",
-    "JetBrains Mono:pixelsize=24:antialias=true:autohint=true"
+    "JetBrains Mono:pixelsize=24:antialias=true:autohint=true",
+    "Hack Nerd Font Mono:pixelsize=24:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
